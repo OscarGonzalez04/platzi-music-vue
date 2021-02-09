2813b365f0a2f782b80307a70f21080cd3254805
Oscar Gonzalez <ochagonzalez04@gmail.com>
2021-02-08 23:10:56 -0600

    Agregando canciones harcodeadas para mostrar lista y total de datos, agregando evento para ejecutar busqueda

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,33 @@
     section.section
       nav.nav.has-shadow
         .container
-          input.input.is-large(type="text", placeholder="Buscar canciones")
-          button.button.is-info.is-large Buscar
-          button.button.is-danger.is-large  
-            span.icon
-              i.fas.fa-times
+          .field.has-addons
+            .control.is-expanded
+              input.input.is-large(
+                type="text", 
+                placeholder="Buscar canciones",
+                v-model="searchQuery"
+              )
+            .control
+              button.button.is-info.is-large(@click="search") Buscar
+            .control
+              button.button.is-danger.is-large  
+                span.icon
+                  i.fas.fa-times
+
+      .container
+        .columns.is-multiline
+          .column.is-full {{ searchMessage }}
+          .column(v-for="track in tracks") {{ track.name }}
 </template>
 
 <script>
+
+const tracks = [
+  { name: 'Muchacha', artist: 'Luis Alberto Spinetta' },
+  { name: 'Hoy aca en el baile', artist: 'El Pepo' },
+  { name: 'I was made for loving you', artist: 'Kiss' }
+];
 
 import PmHome from "./components/layout/PmHome.vue"
 
@@ -21,7 +40,18 @@ export default {
   },
   data () {
     return {
-      msg: 'Hola VUE!'
+      searchQuery: '',
+      tracks: [],
+    }
+  },
+  computed:{
+    searchMessage(){
+      return `Encontrador : ${this.tracks.length}`
+    }
+  },
+  methods:{
+    search(){
+      this.tracks = tracks;
     }
   }
 }
